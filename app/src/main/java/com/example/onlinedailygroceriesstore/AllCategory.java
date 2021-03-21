15e0b5279173ba059d3a67a24e6c870ee0564d5f
andrei <git1@email.com>
2021-03-21 16:23:41 +0100

    Added GridSpacingItemDecoration class
    * Added GridSpacingItemDecoration class for managing the space between the all_category_row_items

--- app/src/main/java/com/example/onlinedailygroceriesstore/AllCategory.java
+++ app/src/main/java/com/example/onlinedailygroceriesstore/AllCategory.java
@@ -5,7 +5,11 @@ import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.res.Resources;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
 
 import com.example.onlinedailygroceriesstore.adapter.AllCategoryAdapter;
 import com.example.onlinedailygroceriesstore.model.AllCategoryModel;
@@ -45,8 +49,53 @@ public class AllCategory extends AppCompatActivity {
     private void setCategoryRecycler(List<AllCategoryModel> allcategoryModelList) {
         RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this, 4);
         AllCategoryRecycler.setLayoutManager(layoutManager);
+        AllCategoryRecycler.addItemDecoration(new GridSpacingItemDecoration(4, dpToPx(16), true));
+        AllCategoryRecycler.setItemAnimator(new DefaultItemAnimator());
         allCategoryAdapter = new AllCategoryAdapter(this,allcategoryModelList);
         AllCategoryRecycler.setAdapter(allCategoryAdapter);
+    }
+
+    public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
+
+        private int spanCount;
+        private int spacing;
+        private boolean includeEdge;
+
+        public GridSpacingItemDecoration(int spanCount, int spacing, boolean includeEdge) {
+            this.spanCount = spanCount;
+            this.spacing = spacing;
+            this.includeEdge = includeEdge;
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            int position = parent.getChildAdapterPosition(view); // item position
+            int column = position % spanCount; // item column
+
+            if (includeEdge) {
+                outRect.left = spacing - column * spacing / spanCount; // spacing - column * ((1f / spanCount) * spacing)
+                outRect.right = (column + 1) * spacing / spanCount; // (column + 1) * ((1f / spanCount) * spacing)
+
+                if (position < spanCount) { // top edge
+                    outRect.top = spacing;
+                }
+                outRect.bottom = spacing; // item bottom
+            } else {
+                outRect.left = column * spacing / spanCount; // column * ((1f / spanCount) * spacing)
+                outRect.right = spacing - (column + 1) * spacing / spanCount; // spacing - (column + 1) * ((1f /    spanCount) * spacing)
+                if (position >= spanCount) {
+                    outRect.top = spacing; // item top
+                }
+            }
+        }
+    }
+
+    /**
+     * Converting dp to pixel
+     */
+    private int dpToPx(int dp) {
+        Resources r = getResources();
+        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
     }
 
 }
