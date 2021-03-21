601b298e60cec6647a2d5b89eb775782848516b5
andrei <git1@email.com>
2021-03-21 10:48:41 +0100

    added Categories recycler view and assets
    * CategoryAdapter.java
    * hooked category recycler view to main activity
    * Category.java model class
    * Vector assets
    * Recycler view

--- app/src/main/java/com/example/onlinedailygroceriesstore/MainActivity.java
+++ app/src/main/java/com/example/onlinedailygroceriesstore/MainActivity.java
@@ -6,7 +6,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.onlinedailygroceriesstore.adapter.CategoryAdapter;
 import com.example.onlinedailygroceriesstore.adapter.DiscountedProductAdapter;
+import com.example.onlinedailygroceriesstore.model.Category;
 import com.example.onlinedailygroceriesstore.model.DiscountedProducts;
 
 import java.util.ArrayList;
@@ -18,20 +20,39 @@ public class MainActivity extends AppCompatActivity {
     DiscountedProductAdapter discountedProductAdapter;
     List<DiscountedProducts> discountedProductsList;
 
+    CategoryAdapter categoryAdapter;
+    List<Category> categoryList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         discountRecyclerView = findViewById(R.id.discountedRecycler);
+        categoryRecyclerView = findViewById(R.id.categoryRecycler);
 
         // adding data to model
         discountedProductsList = new ArrayList<>();
         discountedProductsList.add(new DiscountedProducts(1, R.drawable.discountberry));
         discountedProductsList.add(new DiscountedProducts(2, R.drawable.discountbrocoli));
         discountedProductsList.add(new DiscountedProducts(3, R.drawable.discountmeat));
+        discountedProductsList.add(new DiscountedProducts(4, R.drawable.discountberry));
+        discountedProductsList.add(new DiscountedProducts(5, R.drawable.discountbrocoli));
+        discountedProductsList.add(new DiscountedProducts(6, R.drawable.discountmeat));
+
+        // adding data to model
+        categoryList = new ArrayList<>();
+        categoryList.add(new Category(1, R.drawable.ic_home_fruits));
+        categoryList.add(new Category(2, R.drawable.ic_home_fish));
+        categoryList.add(new Category(3, R.drawable.ic_home_meats));
+        categoryList.add(new Category(4, R.drawable.ic_home_veggies));
+        categoryList.add(new Category(5, R.drawable.ic_home_fruits));
+        categoryList.add(new Category(6, R.drawable.ic_home_fish));
+        categoryList.add(new Category(7, R.drawable.ic_home_meats));
+        categoryList.add(new Category(8, R.drawable.ic_home_veggies));
 
         setDiscountedRecycler(discountedProductsList);
+        setCategoryRecycler(categoryList);
     }
 
     private void setDiscountedRecycler(List<DiscountedProducts> dataList) {
@@ -39,5 +60,12 @@ public class MainActivity extends AppCompatActivity {
         discountRecyclerView.setLayoutManager(layoutManager);
         discountedProductAdapter = new DiscountedProductAdapter(this,dataList);
         discountRecyclerView.setAdapter(discountedProductAdapter);
+    }
+
+    private void setCategoryRecycler(List<Category> categoryDataList) {
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        categoryRecyclerView.setLayoutManager(layoutManager);
+        categoryAdapter = new CategoryAdapter(this,categoryDataList);
+        categoryRecyclerView.setAdapter(categoryAdapter);
     }
 }
