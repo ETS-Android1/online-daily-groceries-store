224fe84db07d73baf5fcd625edd42847ef745036
andrei <git1@email.com>
2021-03-22 18:36:41 +0100

    Added Recently Viewed Items
    * Added Recently Viewed Items section on the Main activity and Recycler View
    * RecentlyViewedAdapter.java
    * hooked recently_item recycler view to the Main activity
    * RecentlyViewed.java model class
    * Added array list of AllCategory model items inside of the Main activity
    * Vector assets
    * Recycler view
    * Added recently_viewed_items.xml layout view

--- app/src/main/java/com/example/onlinedailygroceriesstore/MainActivity.java
+++ app/src/main/java/com/example/onlinedailygroceriesstore/MainActivity.java
@@ -10,8 +10,10 @@ import android.widget.TextView;
 
 import com.example.onlinedailygroceriesstore.adapter.CategoryAdapter;
 import com.example.onlinedailygroceriesstore.adapter.DiscountedProductAdapter;
+import com.example.onlinedailygroceriesstore.adapter.RecentlyViewedAdapter;
 import com.example.onlinedailygroceriesstore.model.Category;
 import com.example.onlinedailygroceriesstore.model.DiscountedProducts;
+import com.example.onlinedailygroceriesstore.model.RecentlyViewed;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
     CategoryAdapter categoryAdapter;
     List<Category> categoryList;
 
+    RecentlyViewedAdapter recentlyViewedAdapter;
+    List<RecentlyViewed> recentlyViewedList;
+
     TextView allCategory;
 
     @Override
@@ -35,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         discountRecyclerView = findViewById(R.id.discountedRecycler);
         categoryRecyclerView = findViewById(R.id.categoryRecycler);
         allCategory = findViewById(R.id.allCategoryImage);
+        recentlyViewedRecycler = findViewById(R.id.recently_item);
 
         allCategory.setOnClickListener(view -> {
             Intent i = new Intent(MainActivity.this, AllCategory.class);
@@ -61,8 +67,16 @@ public class MainActivity extends AppCompatActivity {
         categoryList.add(new Category(7, R.drawable.ic_home_meats));
         categoryList.add(new Category(8, R.drawable.ic_home_veggies));
 
+        // adding data to model
+        recentlyViewedList = new ArrayList<>();
+        recentlyViewedList.add(new RecentlyViewed("Watermelon", "Watermelon has high water content and also provides some fiber.", "€ 7.99", "1", "KG", R.drawable.card4, R.drawable.b4));
+        recentlyViewedList.add(new RecentlyViewed("Papaya", "Papayas are spherical or pear-shaped fruits that can be as long as 20 inches.", "€ 8.59", "1", "KG", R.drawable.card3, R.drawable.b3));
+        recentlyViewedList.add(new RecentlyViewed("Strawberry", "The strawberry is a highly nutritious fruit, loaded with vitamin C.", "€ 3.99", "1", "KG", R.drawable.card2, R.drawable.b1));
+        recentlyViewedList.add(new RecentlyViewed("Kiwi", "Full of nutrients like vitamin C, vitamin K, vitamin E, folate, and potassium.", "€ 3.99", "1", "PC", R.drawable.card1, R.drawable.b2));
+
         setDiscountedRecycler(discountedProductsList);
         setCategoryRecycler(categoryList);
+        setRecentlyViewedRecycler(recentlyViewedList);
     }
 
     private void setDiscountedRecycler(List<DiscountedProducts> dataList) {
@@ -77,5 +91,12 @@ public class MainActivity extends AppCompatActivity {
         categoryRecyclerView.setLayoutManager(layoutManager);
         categoryAdapter = new CategoryAdapter(this,categoryDataList);
         categoryRecyclerView.setAdapter(categoryAdapter);
+    }
+
+    private void setRecentlyViewedRecycler(List<RecentlyViewed> recentlyViewedDataList) {
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recentlyViewedRecycler.setLayoutManager(layoutManager);
+        recentlyViewedAdapter = new RecentlyViewedAdapter(this,recentlyViewedDataList);
+        recentlyViewedRecycler.setAdapter(recentlyViewedAdapter);
     }
 }
